ff6b5c8f8028fd6faeaec4335bb541673a7ed3a1
Shaurya Singh <shaurya.singh@labshare.org>
2020-08-19 09:46:45 -0400

    v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,87 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+
+
+const allTasks = 'allTasks';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'localStorageDemo';
+
+
+export class AppComponent implements OnInit {
+
+  public localGroup: FormGroup;
+  public taskName: FormControl;
+  public isCompleted: FormControl;
+  public tasks: Array<{}>;
+
+
+
+  constructor(private formBuilder: FormBuilder) {
+    this.localGroup = new FormGroup({
+      taskName: new FormControl(''),
+      isCompleted: new FormControl(false)
+    });
+    this.onChanges();
+
+  }
+
+  ngOnInit() {
+    if (localStorage.getItem(allTasks)) {
+      this.tasks = JSON.parse(localStorage.getItem(allTasks));
+      console.log(this.tasks);
+    } else {
+      this.tasks = [];
+    }
+  }
+
+  onModelChange = (model) => { };
+  onModelTouched = (model) => { };
+
+
+
+  registerOnChange(fn: (model) => {}): void {
+    this.onModelChange = fn;
+  }
+
+  registerOnTouched(fn: (model) => {}): void {
+    this.onModelTouched = fn;
+  }
+
+  onChanges(): void {
+    this.localGroup.valueChanges.subscribe(val => {
+      console.log(val);
+    });
+  }
+
+  public addTask() {
+    this.tasks.push(this.localGroup.value);
+    this.localGroup.reset({
+      taskName: '',
+      isCompleted: false
+    });
+
+    localStorage.setItem(allTasks, JSON.stringify(this.tasks));
+  }
+
+  public updateVal(i, event) {
+    if (event) {
+      this.tasks[i]['isCompleted'] = event;
+    }
+    else {
+      this.tasks[i]['isCompleted'] = event;
+    }
+    localStorage.setItem(allTasks, JSON.stringify(this.tasks))
+
+
+  }
 }
+
+
+
+
+
+
